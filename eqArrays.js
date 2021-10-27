a97e55a7320d92b9d16b323f78c22f57a374fb10
Taha Helwani <telhe008@uottawa.ca>
2021-10-27 01:21:39 +0000

    Implemented eqArrays of the lotide project

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// This function eqArrays compares two arrays for a perfect match
+// But it doesn't compare nested arrays or arrays of objects.
+const eqArrays = function (array1, array2) {
+  let isEqual = false;
+  for (let i = 0; i < array1.length; i++) {
+    array1[i] === array2[i] ? isEqual = true : isEqual = false;
+  }
+  return isEqual;
+}
+
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([["1", "2"], ["3", "4"]], [["1", "2"], ["3", "4"]]), false);
