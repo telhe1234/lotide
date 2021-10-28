eca43c7a844fd9f489694c9700fc188b0ddb9104
Taha Helwani <telhe008@uottawa.ca>
2021-10-28 17:47:19 +0000

    Implemented the letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,54 @@
+const eqArrays = function (array1, array2) {
+  let isEqual = false;
+  if (array1.length === 0 && array2.length === 0) {
+    isEqual = true;
+    return isEqual;
+  }
+  if(array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    array1[i] === array2[i] ? isEqual = true : isEqual = false;
+  }
+  return isEqual;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  let resultOfEqArrays = eqArrays(array1, array2);
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+// MAIN FUNCTION
+
+// loop over the characters in the sentencce string literal using for..of
+
+// If the letter doesn't exist in the results object we need to set the value of the current character to an array with the current index of the character
+// else if the letter exists in the results object we need to push the current position into its array literal value
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+
+// TEST CODE
+
+// console.log(letterPositions('hello'));
+
+assertArraysEqual(letterPositions('hello').h, [0]);
+assertArraysEqual(letterPositions('hello').e, [1]);
+assertArraysEqual(letterPositions('hello').l, [2, 3]);
+assertArraysEqual(letterPositions('hello').o, [4]);
