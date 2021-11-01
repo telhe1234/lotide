619f6d8f0488c414f675a8b79637428e781671f9
Taha Helwani <telhe008@uottawa.ca>
2021-11-01 18:56:22 +0000

    Refactored the middle file and created a test file to test it

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-// This function eqArrays compares two arrays for a perfect match.
-// But it doesn't compare nested arrays or arrays of objects.
-const eqArrays = function (array1, array2) {
-  let isEqual = false;
-  if (array1.length === 0 && array2.length === 0) {
-    isEqual = true;
-    return isEqual;
-  }
-  if(array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    array1[i] === array2[i] ? isEqual = true : isEqual = false;
-  }
-  return isEqual;
-};
-
-// This function assertArraysEqual takes in two arrays and console.log(s) an appropriate message to the console
-const assertArraysEqual = function (array1, array2) {
-  const resultOfEqArrays = eqArrays(array1, array2);
-  if (resultOfEqArrays === true) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
 
 // ACTUAL FUNCTION
 const middle = function(array) {
@@ -46,25 +20,6 @@ const middle = function(array) {
     middleMostElementArr.push(arrCopy[indexOftMiddleItem]);
     return middleMostElementArr;
   }
-}
+};
 
-// TEST CODE
-
-// middle([1]) // => []
-// middle([1, 2]) // => []
-
-// middle([1, 2, 3]) // => [2]
-// middle([1, 2, 3, 4, 5]) // => [3]
-
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,23 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// TEST CODE
+
+// middle([1]) // => []
+// middle([1, 2]) // => []
+
+// middle([1, 2, 3]) // => [2]
+// middle([1, 2, 3, 4, 5]) // => [3]
+
+// middle([1, 2, 3, 4]) // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
