6e1720fbc246d89df54c0ada127e2a59174a3b5d
Taha Helwani <telhe008@uottawa.ca>
2021-11-01 03:26:41 +0000

    Implmented the findKey function

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// MAIN FUNCTION
+
+const findKey = function (obj, callback) {
+  const keys = Object.keys(obj);
+  for (let key of keys) {
+    if(callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma'); // => "noma"
