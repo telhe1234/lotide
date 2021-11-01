aae697df6a6ce3edb2668999c36984b7abe47758
Taha Helwani <telhe008@uottawa.ca>
2021-11-01 18:36:03 +0000

    Refactored the assertArraysEqual function and created a test file to test it

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,28 +1,16 @@
-// This function eqArrays compares two arrays for a perfect match.
-// But it doesn't compare nested arrays or arrays of objects.
-const eqArrays = function (array1, array2) {
-  let isEqual = false;
-  if (array1.length === 0 && array2.length === 0) {
-    isEqual = true;
-    return isEqual;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    array1[i] === array2[i] ? isEqual = true : isEqual = false;
-  }
-  return isEqual;
-};
+const eqArrays = require('./eqArrays');
+
 
 // This function assertArraysEqual takes in two arrays and console.log(s) an appropriate message to the console
 const assertArraysEqual = function (actual, expected) {
+  const inspect = require('util').inspect; // <= add this line
   const resultOfEqArrays = eqArrays(actual, expected);
   if (resultOfEqArrays === true) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+
+module.exports = assertArraysEqual;
